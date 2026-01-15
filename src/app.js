fbf4569043c5f0b14b60afe356a38fd1a890ca1d
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-16 03:55:58 +0600

    Update CORS configuration to allow requests from any origin

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(helmet());
 app.use(
     cors({
-      origin: true,
+      origin: '*',
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     })
