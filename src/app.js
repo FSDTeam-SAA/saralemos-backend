4f3d33f43b260348f3319286243779e94c45564d
jarifMohammed <mohammedjarif@protonmail.com>
2026-01-26 01:32:36 +0600

    Add missing origin to allowedOrigins for CORS configuration

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ app.use(mongoSanitize());
 app.use(morgan('combined'));
 const allowedOrigins = [
   "http://localhost:3000",
+  "https://saralemos-backend.onrender.com",
   "https://sara-lemos-client-dashboard.vercel.app",
   "https://saralemos1978-website.vercel.app",
   "http://127.0.0.1:3000",
